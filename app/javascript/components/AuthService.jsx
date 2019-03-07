b0e7a19fca1747c84927d183d7ea995483ccb469
Tanner Hartwig <tanner.hartwig57@gmail.com>
2019-03-07 14:27:37 -0600

    Setup AuthService for frontend components

---
+++ app/javascript/components/AuthService.jsx
@@ -0,0 +1,73 @@
+import decode from "jwt-decode";
+
+class AuthService {
+  constructor(domain) {
+    this.domain = domain || "http://localhost:5000";
+  }
+
+  loggedIn = () => {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  };
+
+  login = async (email, password) => {
+    const response = await this.fetch(`${this.domain}/login`, {
+      method: "POST",
+      body: JSON.stringify({
+        auth: {
+          email,
+          password
+        }
+      })
+    });
+
+    if (response.status === 201) {
+      const data = await response.json();
+      this.setToken(data.jwt);
+      return true;
+    }
+
+    return false;
+  };
+
+  logout = () => {
+    localStorage.removeItem("id_token");
+  };
+
+  setToken = token => {
+    localStorage.setItem("id_token", token);
+  };
+
+  getToken = () => {
+    return localStorage.getItem("id_token");
+  };
+
+  isTokenExpired = token => {
+    try {
+      const decoded = decode(token);
+      return decoded.exp < Date.now() / 1000;
+    } catch (error) {
+      return true;
+    }
+  };
+
+  fetch = (url, options) => {
+    const headers = {
+      // TODO: add new quote-props: consistent rule
+      //       with next release of prettier (Yay!)
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    };
+
+    if (this.loggedIn()) {
+      headers.Authorization = `Bearer ${this.getToken()}`;
+    }
+
+    return fetch(url, {
+      headers,
+      ...options
+    });
+  };
+}
+
+export default AuthService;
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
+  post 'login' => 'user_token#create'
+
   get '/users/current' => 'users#current'
   post '/users/create' => 'users#create'
-  
-  post 'user_token' => 'user_token#create'
+
+  get '*path', to: 'home#react_app', constraints: -> (request) do
+    !request.xhr? && request.format.html?
+  end
 end
