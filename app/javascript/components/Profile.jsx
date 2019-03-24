3c59c59f0a8989c3117c946d702d81d00a572323
Tanner Hartwig <tanner.hartwig57@gmail.com>
2019-03-23 20:21:29 -0500

    WorkoutService to handle api calls

--- app/javascript/components/Profile.jsx
+++ app/javascript/components/Profile.jsx
@@ -2,12 +2,35 @@ import React from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 import ProfileStyles from "../styles/Profile";
+import WorkoutService from "./WorkoutService";
 
-const Profile = props => {
-  const { classes } = props;
+class Profile extends React.Component {
+  state = {
+    exerciseNames: [],
+    workouts: []
+  };
 
-  return <h1>Profile Page</h1>;
-};
+  async componentDidMount() {
+    const workouts = await WorkoutService.getWorkouts();
+    const exerciseNames = await WorkoutService.getExerciseNames();
+
+    this.setState({
+      exerciseNames,
+      workouts
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { exerciseNames, workouts } = this.state;
+
+    return (
+      <div>
+        <h1>Profile Page</h1>
+      </div>
+    );
+  }
+}
 
 Profile.propTypes = {
   classes: PropTypes.object.isRequired // eslint-disable-line react/forbid-prop-types
---
+++ app/javascript/components/WorkoutService.jsx
@@ -0,0 +1,54 @@
+import Auth from "./AuthService";
+
+class WorkoutService {
+  constructor(domain) {
+    this.domain = domain || "http://localhost:5000";
+  }
+
+  getWorkouts = async () => {
+    const response = await this.fetch("/workouts", {
+      method: "GET"
+    });
+
+    if (response.status === 200) {
+      const workouts = await response.json();
+      return workouts;
+    }
+
+    return [];
+  };
+
+  getExerciseNames = async () => {
+    const response = await this.fetch("/exercise_names", {
+      method: "GET"
+    });
+
+    if (response.status === 200) {
+      const exerciseNames = await response.json();
+      return exerciseNames;
+    }
+
+    return [];
+  };
+
+  fetch = (url, options) => {
+    const fullUrl = `${this.domain}${url}`;
+    const headers = {
+      // TODO: add new quote-props: consistent rule
+      //       with next release of prettier (Yay!)
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    };
+
+    if (Auth.loggedIn()) {
+      headers.Authorization = `Bearer ${Auth.getToken()}`;
+    }
+
+    return fetch(fullUrl, {
+      headers,
+      ...options
+    });
+  };
+}
+
+export default new WorkoutService();
